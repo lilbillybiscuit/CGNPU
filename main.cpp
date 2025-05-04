28a2d3d40b855d0ce8f327d53f11da120fe0d2d8
lilbillybiscuit <qianbill2014@outlook.com>
2025-05-04 15:30:31 -0400

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define NS_PRIVATE_IMPLEMENTATION
+#define CA_PRIVATE_IMPLEMENTATION
+#define MTL_PRIVATE_IMPLEMENTATION
+#include "metal-cpp/Foundation/Foundation.hpp"
+#include "metal-cpp/Metal/Metal.hpp"
+#include "metal-cpp/QuartzCore/QuartzCore.hpp"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
